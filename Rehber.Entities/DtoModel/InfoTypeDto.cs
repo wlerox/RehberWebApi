ddc9c7a1b18158a18650f6d7425e958cadf89011
wlerox <wlerox@DESKTOP-PBUIN17>
2022-02-24 00:16:18 +0300

    entities_update

--- Rehber.Entities/DtoModel/InfoTypeDto.cs
+++ Rehber.Entities/DtoModel/InfoTypeDto.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Rehber.Entities.DtoModel
 {
     public class InfoTypeDto
     {
+        [Required]
         public int InfoTypeID { get; set; }
+        [Required]
         public string InfoTypeName { get; set; }
     }
 }
